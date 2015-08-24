964b71a8414527530d3500fbcca3bdf914ea985a
Shawn Ma <qsma@thoughtworks.com>
2015-08-24 16:20:04 +0800

    update the config

--- 7.7.0/files/sentry.conf.py
+++ 7.7.0/files/sentry.conf.py
@@ -1,49 +1,31 @@
 # This file is just Python, with a touch of Django which means
 # you can inherit and tweak settings to your hearts content.
 from sentry.conf.server import *
+from decouple import config
 
 import os.path
 
 CONF_ROOT = os.path.dirname(__file__)
 
+DATA_DIR  = config('SENTRY_DATA_DIR', default='/sentry/data')
+
 DATABASES = {
     'default': {
-        'ENGINE':   os.environ.get('SENTRY_DB_ENGINE', 'django.db.backends.sqlite3'),
-        'HOST':     os.environ.get('SENTRY_DB_HOST',   ''),
-        'PORT':     os.environ.get('SENTRY_DB_PORT',   ''),
-        'NAME':     os.environ.get('SENTRY_DB_NAME',   '/sentry/sentry.db'),
-        'USER':     os.environ.get('SENTRY_DB_USER',   'postgres'),
-        'PASSWORD': os.environ.get('SENTRY_DB_PASS',   ''),
+        'ENGINE':   config('SENTRY_DB_ENGINE', default='django.db.backends.sqlite3'),
+        'HOST':     config('SENTRY_DB_HOST',   default=''),
+        'PORT':     config('SENTRY_DB_PORT',   default=''),
+        'NAME':     config('SENTRY_DB_NAME',   default='/sentry/sentry.db'),
+        'USER':     config('SENTRY_DB_USER',   default='postgres'),
+        'PASSWORD': config('SENTRY_DB_PASS',   default=''),
     }
 }
-
-# You should not change this setting after your database has been created
-# unless you have altered all schemas first
-SENTRY_USE_BIG_INTS = True
-
-# If you're expecting any kind of real traffic on Sentry, we highly recommend
-# configuring the CACHES and Redis settings
-
-
-
-# The administrative email for this installation.
-# Note: This will be reported back to getsentry.com as the point of contact. See
-# the beacon documentation for more information. This **must** be a string.
-SENTRY_ADMIN_EMAIL = os.environ.get('SENTRY_ADMIN_EMAIL', '')
-
-# Instruct Sentry that this install intends to be run by a single organization
-# and thus various UI optimizations should be enabled.
-SENTRY_SINGLE_ORGANIZATION = True
-
-# Should Sentry allow users to create new accounts?
-SENTRY_FEATURES['auth:register'] = False
 
 #########
 # Redis #
 #########
-REDIS_HOST = os.environ.get('SENTRY_REDIS_HOST', '127.0.0.1')
-REDIS_PORT = os.environ.get('SENTRY_REDIS_PORT', '6379')
-REDIS_DB   = os.environ.get('SENTRY_REDIS_DB', '0')
+REDIS_HOST = config('SENTRY_REDIS_HOST', default='redis')
+REDIS_PORT = config('SENTRY_REDIS_PORT', default=6379, cast=int)
+REDIS_DB   = config('SENTRY_REDIS_DB',   default=0,    cast=int)
 
 # Generic Redis configuration used as defaults for various things including:
 # Buffers, Quotas, TSDB
@@ -62,18 +44,7 @@ SENTRY_REDIS_OPTIONS = {
 # Cache #
 #########
 
-CACHE_TYPE = os.environ.get('SENTRY_CACHE_TYPE', 'redis')
-
-if CACHE_TYPE == 'memcached':
-    CACHES = {
-        'default': {
-             'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-             'LOCATION': ['127.0.0.1:11211'],
-        }
-    }
-    SENTRY_CACHE = 'sentry.cache.django.DjangoCache'
-else:
-    SENTRY_CACHE = 'sentry.cache.redis.RedisCache'
+SENTRY_CACHE = 'sentry.cache.redis.RedisCache'
 
 #########
 # Queue #
@@ -83,8 +54,11 @@ else:
 # information on configuring your queue broker and workers. Sentry relies
 # on a Python framework called Celery to manage queues.
 
-CELERY_ALWAYS_EAGER = False
-BROKER_URL = 'redis://%s:%s/%s' % (REDIS_HOST, REDIS_PORT, REDIS_DB)
+# You can enable queueing of jobs by turning off the always eager setting:
+CELERY_ALWAYS_EAGER = config('CELERY_ALWAYS_EAGER', default=False, cast=bool)
+CELERY_BROKER_URL   = 'redis://%s:%d/%d' % (REDIS_HOST, REDIS_PORT, REDIS_DB)
+
+BROKER_URL = config('SENTRY_BROKER_URL', default=CELERY_BROKER_URL)
 
 ###############
 # Rate Limits #
@@ -141,19 +115,21 @@ SENTRY_FILESTORE_OPTIONS = {
 ##############
 
 # You MUST configure the absolute URI root for Sentry:
-SENTRY_URL_PREFIX = os.environ.get('SENTRY_SENTRY_URL_PREFIX', 'http://sentry.example.com') 
+SENTRY_URL_PREFIX = config('SENTRY_URL_PREFIX' , default='http://sentry.example.com')
 
 # If you're using a reverse proxy, you should enable the X-Forwarded-Proto
 # header and uncomment the following settings
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SESSION_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = config('SENTRY_SECURE_PROXY_SSL_HEADER', default=None, cast=lambda x: tuple(x.split(',')) if x else None)
 
-SENTRY_WEB_HOST = '0.0.0.0'
-SENTRY_WEB_PORT = 9000
+SENTRY_WEB_HOST = config('SENTRY_WEB_HOST', default='0.0.0.0')
+SENTRY_WEB_PORT = config('SENTRY_WEB_PORT', default=9000, cast=int)
 SENTRY_WEB_OPTIONS = {
-    # 'workers': 3,  # the number of gunicorn workers
-    # 'secure_scheme_headers': {'X-FORWARDED-PROTO': 'https'},
+    'workers': config('SENTRY_WORKERS', default=3, cast=int),  # the number of gunicorn workers
+    'secure_scheme_headers': {'X-FORWARDED-PROTO': 'https'},
 }
+
+# allows JavaScript clients to submit cross-domain error reports. Useful for local development
+SENTRY_ALLOW_ORIGIN = config('SENTRY_ALLOW_ORIGIN', default=None)
 
 ###############
 # Mail Server #
@@ -162,24 +138,50 @@ SENTRY_WEB_OPTIONS = {
 # For more information check Django's documentation:
 #  https://docs.djangoproject.com/en/1.3/topics/email/?from=olddocs#e-mail-backends
 
-EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST          = os.environ.get('SENTRY_EMAIL_HOST', 'localhost')
-EMAIL_HOST_USER     = os.environ.get('SENTRY_EMAIL_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('SENTRY_EMAIL_PASS', '')
-EMAIL_PORT          = os.environ.get('SENTRY_EMAIL_PORT', '25')
-EMAIL_USE_TLS       = bool(os.environ.get('SENTRY_EMAIL_USE_TLS', 'False'))
+EMAIL_BACKEND       = config('SENTRY_EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
+EMAIL_HOST          = config('SENTRY_EMAIL_HOST',    default='localhost')
+EMAIL_HOST_USER     = config('SENTRY_EMAIL_USER',    default='')
+EMAIL_HOST_PASSWORD = config('SENTRY_EMAIL_PASS',    default='')
+EMAIL_PORT          = config('SENTRY_EMAIL_PORT',    default=25,    cast=int)
+EMAIL_USE_TLS       = config('SENTRY_EMAIL_USE_TLS', default=False, cast=bool)
 
 # The email address to send on behalf of
-SERVER_EMAIL        = os.environ.get('SENTRY_SERVER_EMAIL', 'root@localhost')
-
-# If you're using mailgun for inbound mail, set your API key and configure a
-# route to forward to /api/hooks/mailgun/inbound/
-MAILGUN_API_KEY     = os.environ.get('SENTRY_MAILGUN_API_KEY', '')
+SERVER_EMAIL        = config('SENTRY_SERVER_EMAIL', default='root@localhost')
 
 ########
 # etc. #
 ########
 
+SENTRY_PUBLIC = config('SENTRY_PUBLIC', default=False, cast=bool)
+
+SENTRY_BEACON = config('SENTRY_BEACON', default=True,  cast=bool)
+
+SENTRY_FEATURES['auth:register'] = config('SENTRY_ALLOW_REGISTRATION', default=False, cast=bool)
+
+SENTRY_ADMIN_EMAIL = config('SENTRY_ADMIN_EMAIL', default='')
+
+
 # If this file ever becomes compromised, it's important to regenerate your SECRET_KEY
 # Changing this value will result in all current sessions being invalidated
-SECRET_KEY = '5V1r7G3vQBr2gbU2tgDBNitmEuQ82JMtt7N0adHSx2IllzQhRK6HrA=='
+SECRET_KEY = config('SECRET_KEY', default='5V1r7G3vQBr2gbU2tgDBNitmEuQ82JMtt7N0adHSx2IllzQhRK6HrA==')
+
+# http://twitter.com/apps/new
+TWITTER_CONSUMER_KEY      = config('TWITTER_CONSUMER_KEY', default='')
+TWITTER_CONSUMER_SECRET   = config('TWITTER_CONSUMER_SECRET', default='')
+
+# http://developers.facebook.com/setup/
+FACEBOOK_APP_ID           = config('FACEBOOK_APP_ID', default='')
+FACEBOOK_API_SECRET       = config('FACEBOOK_API_SECRET', default='')
+
+# https://github.com/settings/applications/new
+GITHUB_APP_ID             = config('GITHUB_APP_ID', default='')
+GITHUB_API_SECRET         = config('GITHUB_API_SECRET', default='')
+
+# https://trello.com/1/appKey/generate
+TRELLO_API_KEY            = config('TRELLO_API_KEY', default='')
+TRELLO_API_SECRET         = config('TRELLO_API_SECRET', default='')
+
+# https://confluence.atlassian.com/display/BITBUCKET/OAuth+Consumers
+BITBUCKET_CONSUMER_KEY    = config('BITBUCKET_CONSUMER_KEY', default='')
+BITBUCKET_CONSUMER_SECRET = config('BITBUCKET_CONSUMER_SECRET', default='')
+
